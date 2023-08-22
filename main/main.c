678f1b5231ac9e1c94741883ddb0868835edc7a8
= <f7lipe@me.com>
2023-08-22 09:00:23 -0300

    project setup

--- main/main.c
+++ main/main.c
@@ -1,31 +1,45 @@
 #include "src/ui/ui.h"
 #include "src/hardware/microphone/mic.h"
 #include "src/dsp/signal.h"
+#include "esp_log.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
 
-#define BUFFER_SIZE 1024
-#define SAMPLE_RATE 16000
+#define N_WINDOW_ELEMENTS 1024
+#define POTENCY_WINDOW_SIZE 32
+#define MUSIC_SIZE 1024
+#define FRAME_RATE 16000
+#define NOISE_THRESHOLD 2
+
+int16_t window[N_WINDOW_ELEMENTS];
+int16_t windowModified[N_WINDOW_ELEMENTS];
+int16_t music[MUSIC_SIZE];
+int potencies[POTENCY_WINDOW_SIZE];
+int potencyActualIndex = 0;
+int newPotency = 0;
+int signalVelocity = 1;
+int musicIndex = 0;
 
 void app_main(void) {
-    ui_init();
 
+    ui_init();
     mic_init();
 
-    float mic_buffer[BUFFER_SIZE];
-    float processed_data[BUFFER_SIZE];
+    int16_t buffer[N_WINDOW_ELEMENTS];
 
     while (1) {
+        mic_read_data(buffer, N_WINDOW_ELEMENTS);
 
-        mic_read(mic_buffer, BUFFER_SIZE);
+        // Copie o buffer para a janela (window) e realize as operações de processamento de sinal
+        // com as funções definidas em signal.c
 
-        process_signal(mic_buffer, processed_data, BUFFER_SIZE);
+        bool presence = signal_detect_presence(buffer);
+        float higherPowerFrequency = signal_find_higher_power_frequency(buffer);
 
-        int signal_present = detect_presence(processed_data, BUFFER_SIZE, 0.1);
-        ui_show_signal_presence(signal_present);
-        
-        float frequency = calculate_peak_frequency(processed_data, BUFFER_SIZE, SAMPLE_RATE);
-        ui_display_frequency(frequency);
-
-        // TODO - Implementar próximas tarefas do projeto
-        // ...
+        // Atualize a interface do usuário
+        ui_update_display(higherPowerFrequency);
+        ui_update_led(presence);
     }
+    free(buffer);
 }
