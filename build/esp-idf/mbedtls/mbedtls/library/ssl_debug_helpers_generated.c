b58b02b94de55f5491de79cf4ad0093deb3b0212
= <f7lipe@me.com>
2023-09-10 23:20:33 -0300

    Remover a pasta build do controle de versão

--- build/esp-idf/mbedtls/mbedtls/library/ssl_debug_helpers_generated.c
+++
@@ -1,1 +0,0 @@
-/Users/f7lipe/esp/esp-idf/components/mbedtls/mbedtls/library/ssl_debug_helpers_generated.c
--- build/esp-idf/mbedtls/mbedtls/library/version_features.c
+++
@@ -1,1 +0,0 @@
-/Users/f7lipe/esp/esp-idf/components/mbedtls/mbedtls/library/version_features.c
