727815bdf9d9fae3d21537d912e32590836685ff
= <f7lipe@me.com>
2023-09-18 12:03:00 -0300

    misc: mock buffer

--- main/main.c
+++ main/main.c
@@ -10,21 +10,27 @@
 #include "src/ui/ui.h"
 #include <time.h>
 
-// Definir uma estrutura para armazenar informações da tarefa
 struct TaskInfo
 {
-    const char *name;      // Nome da tarefa
-    void (*task)();        // Função da tarefa
-    double execution_time; // Tempo de execução
+    const char *name;     
+    void (*task)();       
+    double execution_time;
 };
 
-// Declarações
 #define N_WINDOW_ELEMENTS 1024
 int signalVelocity = 1;
 int16_t buffer[N_WINDOW_ELEMENTS];
+int16_t mockedBuffer[N_WINDOW_ELEMENTS];
+
+void generate_simulated_signal() {
+    for (int i = 0; i < N_WINDOW_ELEMENTS; i++) {
+        mockedBuffer[i] = (int16_t)(rand() % 1024);
+    }
+}
 
 bool init(void)
 {
+    generate_simulated_signal();
     return mic_init() /*&& bluetooth_init()*/ && ui_init();
 }
 
@@ -41,22 +47,22 @@ void read_data()
 
 void modify_velocity()
 {
-    signal_modify_velocity(buffer, N_WINDOW_ELEMENTS, signalVelocity);
+    signal_modify_velocity(mockedBuffer, N_WINDOW_ELEMENTS, signalVelocity);
 }
 
 void detect_presence()
 {
-    signal_detect_presence(buffer);
+    signal_detect_presence(mockedBuffer);
 }
 
 void higher_power_frequency()
 {
-    signal_find_higher_power_frequency(buffer);
+    signal_find_higher_power_frequency(mockedBuffer);
 }
 
 void update_display()
 {
-     ui_update_display(343.1);
+     ui_update_display(mockedBuffer[0]);
 }
 
 void update_led()
@@ -71,7 +77,7 @@ void app_main(void)
     {
         ESP_LOGI("MAIN", "Initialization successful");
 
-        #define N_REPETITIONS 10
+        #define N_REPETITIONS 1
         struct TaskInfo tasks[] = {
             {"Leitura do Microfone", read_data, 0.0},
             {"Modificação da Velocidade do Sinal", modify_velocity, 0.0},
@@ -90,7 +96,6 @@ void app_main(void)
             for (int j = 0; j < N_REPETITIONS; j++)
             {
                 tasks[i].task();
-                signalVelocity++;
             }
             end_time = clock();
 
